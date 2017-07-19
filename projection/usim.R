d2f7c2b822028cd698328f835cef78c3b877d969
virgesmith <a.p.smith@leeds.ac.uk>
2017-07-19 11:51:06 +0100

    - first cut of (optional) code to generate synthetic population. - added precomputed synthetic population

--- projection/usim.R
+++ projection/usim.R
@@ -1,23 +1,23 @@
-# usim.R
+# usim.R 
+# This file synthesises a population in: MSOA, Sex, Age(single year), Ethnicity
+# The input data are two tables containing:
+# Aggregates of Persons per MSOA, Sex, Age(band) and Ethnicity
+# Aggregates of Persons per MSOA, Sex, Age(single year)
 
-# source this file to run the microsimulation
-
-# load in the aggregates derived from census data
+# source this file to run the microsynthesis that is used as tbe basis for the projection microsimulation
+# Running this code is optional, a synthetic population is provided in projection/data/synpop.csv
 
 # load dependencies
 library(data.table)
 
-#
-setwd("~/dev/usim_demog/projection/")
+# Ensure your working directory is the project root so that the data files can be found, e.g.
+#setwd("~/dev/usim_demog/")
 
-# read in data:
-# population by sex, ageBand, ethnicity
-sexAgeEth=as.data.table(read.csv("./data/sexAgeEth.csv", stringsAsFactors = F))
+# load in the aggregates derived from census data# population by sex, age band, ethnicity
+sexAgeEth=as.data.table(read.csv("./projection/data/sexAgeEth.csv", stringsAsFactors = F))
 # population by single year of age
-sexAgeYear=as.data.table(read.csv("./data/sexAgeYear.csv", stringsAsFactors = F))
+sexAgeYear=as.data.table(read.csv("./projection/data/sexAgeYear.csv", stringsAsFactors = F))
 
-
-# this is slightly pointless as the data is already there
 msoa=aggregate(sexAgeEth$Persons, by=list(sexAgeEth$MSOA), FUN=sum)
 sex=aggregate(sexAgeEth$Persons, by=list(sexAgeEth$Sex), FUN=sum)
 age=aggregate(sexAgeEth$Persons, by=list(sexAgeEth$Age), FUN=sum)
@@ -27,39 +27,63 @@ ageLookup=data.table(Band=c("0-4","5-7","8-9","10-14","15","16-17","18-19","20-2
                      LBound=c(0,5,8,10,15,16,18,20,25,30,35,40,45,50,55,60,65,70,75,80,85),
                      UBound=c(4,7,9,14,15,17,19,24,29,34,39,44,49,54,59,64,69,74,79,84,999))
 
-# microsim to get sex-age-eth? expanding single year
+# overall population
+n = sum(sexAgeYear$Persons)
+
+synpop=data.table(MSOA=rep("",n), Sex=rep("",n), Age=rep(-1,n), Ethnicity=rep("",n))
+
+index = 1L
+
+# microsim to get sex-age-eth expanded to single year of age, preserving marginal totals in area, sex, age band and ethnicity
 for (a in msoa$Group.1) {
   for (s in sex$Group.1) {
     for (b in age$Group.1) {
+      # marginal labels
       l1 = sexAgeEth[MSOA==a & Sex==s & Age==b]$Ethnicity
-      m1 = sexAgeEth[MSOA==a & Sex==s & Age==b]$Persons
       l2 = sexAgeYear[MSOA==a & Sex==s & Age >= ageLookup[Band==b]$LBound & Age <= ageLookup[Band==b]$UBound]$Age
-      m2 = sexAgeYear[MSOA==a & Sex==s & Age >= 0 & Age < 5]$Persons
-      res = humanleague::synthPop(list(m1,m2))
+      # marginal frequencies
+      m1 = sexAgeEth[MSOA==a & Sex==s & Age==b]$Persons
+      m2 = sexAgeYear[MSOA==a & Sex==s & Age >= ageLookup[Band==b]$LBound & Age <= ageLookup[Band==b]$UBound]$Persons
+      # microsynthesis
+      if (sum(m1)>0) {
+        res = humanleague::synthPop(list(m1,m2))
+        for (i in 1:nrow(res$pop)) {
+          set(synpop,index,"MSOA", a)
+          set(synpop,index,"Sex", s)
+          set(synpop,index,"Age", l2[res$pop$C1[i]+1])
+          set(synpop,index,"Ethnicity", l1[res$pop$C0[i]+1])
+          index = index + 1L
+        }
+      }
     }
   }
 }
 
+# do some spot-checks on the population to ensure it's consistent
 
-# mortality rates (TODO revert back to single year)
-mortality=as.data.table(read.csv("./data/TowerHamletsMortality.csv", stringsAsFactors = F))
 
-# this is slightly pointless as the data is already there
-msoa=aggregate(sexAgeEth$Persons, by=list(sexAgeEth$MSOA), FUN=sum)
-sex=aggregate(sexAgeEth$Persons, by=list(sexAgeEth$Sex), FUN=sum)
-age=aggregate(sexAgeEth$Persons, by=list(sexAgeEth$Age), FUN=sum)
-eth=aggregate(sexAgeEth$Persons, by=list(sexAgeEth$Ethnicity), FUN=sum)
+# save data
+write.csv(synpop,"./projection/data/synpop.csv", row.names = F)
 
-# Pointless, but illustrates how we dont preserve totals for msoa-sex-age-eth combination
-res=humanleague::synthPop(list(msoa$x,sex$x,age$x,eth$x))
-
-# Make the categories more readable
-# indices start at zero in the output so add one
-res$pop$C0=msoa$Group.1[res$pop$C0+1]
-res$pop$C1=sex$Group.1[res$pop$C1+1]
-res$pop$C2=age$Group.1[res$pop$C2+1]
-res$pop$C3=eth$Group.1[res$pop$C3+1]
-
-pop=nrow(sexAgeEth)
+# # mortality rates (TODO revert back to single year)
+# mortality=as.data.table(read.csv("./projection/data/TowerHamletsMortality.csv", stringsAsFactors = F))
+# 
+# # this is slightly pointless as the data is already there
+# msoa=aggregate(sexAgeEth$Persons, by=list(sexAgeEth$MSOA), FUN=sum)
+# sex=aggregate(sexAgeEth$Persons, by=list(sexAgeEth$Sex), FUN=sum)
+# age=aggregate(sexAgeEth$Persons, by=list(sexAgeEth$Age), FUN=sum)
+# eth=aggregate(sexAgeEth$Persons, by=list(sexAgeEth$Ethnicity), FUN=sum)
+# 
+# # Pointless, but illustrates how we dont preserve totals for msoa-sex-age-eth combination
+# res=humanleague::synthPop(list(msoa$x,sex$x,age$x,eth$x))
+# 
+# # Make the categories more readable
+# # indices start at zero in the output so add one
+# res$pop$C0=msoa$Group.1[res$pop$C0+1]
+# res$pop$C1=sex$Group.1[res$pop$C1+1]
+# res$pop$C2=age$Group.1[res$pop$C2+1]
+# res$pop$C3=eth$Group.1[res$pop$C3+1]
+# 
+# pop=nrow(sexAgeEth)
 
 
