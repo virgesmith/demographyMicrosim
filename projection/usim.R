802f22603daa26a3cdcea0191acf1677bc80a3aa
virgesmith <a.p.smith@leeds.ac.uk>
2017-07-19 12:33:09 +0100

    missed some updates

--- projection/usim.R
+++ projection/usim.R
@@ -23,6 +23,7 @@ sex=aggregate(sexAgeEth$Persons, by=list(sexAgeEth$Sex), FUN=sum)
 age=aggregate(sexAgeEth$Persons, by=list(sexAgeEth$Age), FUN=sum)
 eth=aggregate(sexAgeEth$Persons, by=list(sexAgeEth$Ethnicity), FUN=sum)
 
+# mapping from age bands to age ranges
 ageLookup=data.table(Band=c("0-4","5-7","8-9","10-14","15","16-17","18-19","20-24","25-29","30-34","35-39","40-44","45-49","50-54","55-59","60-64","65-69","70-74","75-79","80-84","85+"),
                      LBound=c(0,5,8,10,15,16,18,20,25,30,35,40,45,50,55,60,65,70,75,80,85),
                      UBound=c(4,7,9,14,15,17,19,24,29,34,39,44,49,54,59,64,69,74,79,84,999))
@@ -60,30 +61,26 @@ for (a in msoa$Group.1) {
 }
 
 # do some spot-checks on the population to ensure it's consistent
+# ensure all rows populated
+stopifnot(nrow(synpop[Age==-1]) == 0)
+# check sex totals match
+stopifnot(nrow(synpop[Sex=="M"]) == sum(sexAgeYear[Sex=="M"]$Persons))
+stopifnot(nrow(synpop[Sex=="F"]) == sum(sexAgeYear[Sex=="F"]$Persons))
+# check some age totals match
+stopifnot(nrow(synpop[Age==8]) == sum(sexAgeYear[Age==8]$Persons))
+stopifnot(nrow(synpop[Age==48]) == sum(sexAgeYear[Age==48]$Persons))
+# check some ethnicity totals match
+stopifnot(nrow(synpop[Ethnicity=="OTH"]) == sum(sexAgeEth[Ethnicity=="OTH"]$Persons))
+stopifnot(nrow(synpop[Ethnicity=="BLC"]) == sum(sexAgeEth[Ethnicity=="BLC"]$Persons))
+stopifnot(nrow(synpop[Ethnicity=="BAN"]) == sum(sexAgeEth[Ethnicity=="BAN"]$Persons))
+# check some MSOA totals match
+stopifnot(nrow(synpop[MSOA=="E02000864"]) == sum(sexAgeEth[MSOA=="E02000864"]$Persons))
+stopifnot(nrow(synpop[MSOA=="E02000888"]) == sum(sexAgeEth[MSOA=="E02000888"]$Persons))
+stopifnot(nrow(synpop[MSOA=="E02006854"]) == sum(sexAgeEth[MSOA=="E02006854"]$Persons))
 
 
 # save data
 write.csv(synpop,"./projection/data/synpop.csv", row.names = F)
 
-# # mortality rates (TODO revert back to single year)
-# mortality=as.data.table(read.csv("./projection/data/TowerHamletsMortality.csv", stringsAsFactors = F))
-# 
-# # this is slightly pointless as the data is already there
-# msoa=aggregate(sexAgeEth$Persons, by=list(sexAgeEth$MSOA), FUN=sum)
-# sex=aggregate(sexAgeEth$Persons, by=list(sexAgeEth$Sex), FUN=sum)
-# age=aggregate(sexAgeEth$Persons, by=list(sexAgeEth$Age), FUN=sum)
-# eth=aggregate(sexAgeEth$Persons, by=list(sexAgeEth$Ethnicity), FUN=sum)
-# 
-# # Pointless, but illustrates how we dont preserve totals for msoa-sex-age-eth combination
-# res=humanleague::synthPop(list(msoa$x,sex$x,age$x,eth$x))
-# 
-# # Make the categories more readable
-# # indices start at zero in the output so add one
-# res$pop$C0=msoa$Group.1[res$pop$C0+1]
-# res$pop$C1=sex$Group.1[res$pop$C1+1]
-# res$pop$C2=age$Group.1[res$pop$C2+1]
-# res$pop$C3=eth$Group.1[res$pop$C3+1]
-# 
-# pop=nrow(sexAgeEth)
 
 
