64f898b0bbc84108451a5a9cfdaaff7bde3ec0f5
virgesmith <a.p.smith@leeds.ac.uk>
2017-11-13 10:47:09 +0000

    Tweaks to figures

--- R/map.R
+++ R/map.R
@@ -17,13 +17,19 @@ map = function(data, minColour = "#0080FF", maxColour= "#FF8000") {
 
   # makes use of the following lazy-loaded package data: msoa
 
+  # Scale up growth to display as percentage
+  data$Value = data$Value * 100.0
+
   map = leaflet() %>% addTiles(urlTemplate = "//a.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png")#, options=tileOptions(opacity=0.5))
   # Use of these tiles requires an acknowledgement
   cat("Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL\n")
 
   pal=colorNumeric(c(minColour,maxColour),c(min(data$Value),max(data$Value)))
 
-  map = map %>% addPolygons(data = msoa[msoa$code==data$MSOA,], fillColor = pal(data$Value), fillOpacity=.2, weight=1, color = "black")
+  map = map %>% addPolygons(data = msoa[msoa$code==data$MSOA,], fillColor = pal(data$Value), fillOpacity=.2, weight=1, color = "black") %>%
+              addLegend("bottomleft", pal = pal, values = c(min(data$Value),max(data$Value)),
+              labFormat = labelFormat(suffix = "%"),
+              opacity = .5)
 
   return(map)
 }
